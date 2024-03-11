4b02b7a3a573692b7b51dc2e1b0804660ff73885
JAYANTHBALRAJ <113664118+JAYANTHBALRAJ@users.noreply.github.com>
2024-03-11 12:30:07 +0530

    Delete day 11/src directory

--- day 11/src/main/java/com/example/demo/ResourceNotFoundException/UserAlreadyExistingException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.ResourceNotFoundException;
-
-public class UserAlreadyExistingException extends RuntimeException {
-    public UserAlreadyExistingException(String message) {
-        super(message);
-    }
-}
