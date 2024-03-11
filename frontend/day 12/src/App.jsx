01c4d7f2d76ad78b21913bdd2ae6b1f6314aed19
JAYANTHBALRAJ <113664118+JAYANTHBALRAJ@users.noreply.github.com>
2024-03-11 11:59:35 +0530

    Add files via upload

---
+++ frontend/day 12/src/App.jsx
@@ -0,0 +1,15 @@
+// app.jsx
+// import React from 'react';
+//import HomePage from './components/Homepage';
+import Login from './components/login';
+
+const App = () => {
+  return (
+    <div>
+      <Login/>
+
+    </div>
+  );
+};
+
+export default App;
